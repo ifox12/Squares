0a671bc72960a53b14431c4f384182776986ae52
Tim Petschauer <tim.petschauer@posteo.net>
2020-03-02 08:21:23 +0100

    Fixed resize bug

--- src/Square.java
+++ src/Square.java
@@ -7,12 +7,10 @@ public class Square {
     private int height = 50;
     private int x;
     private int y;
-    private Point base;
 
     public Square(int x, int y) {
         this.x = x;
         this.y = y;
-        this.base = new Point(x, y);
     }
 
     public void reposition(int x, int y) {
@@ -31,7 +29,6 @@ public class Square {
         g.drawString("y: " + y, 10, 25);
         g.drawString("w: " + width, 10, 40);
         g.drawString("h: " + height, 10, 55);
-        g.drawString("baseSquare: (" + base.x + "," + base.y + ")", 10, 70);
     }
 
     public Rectangle getMoveRectangle() {
@@ -51,12 +48,12 @@ public class Square {
     }
 
     public void resize(Point newPointerPosition, Point squareBase) {
-        this.base = squareBase;
-
         if (newPointerPosition.x > squareBase.x) {
+            this.x = squareBase.x;
             this.width = newPointerPosition.x - squareBase.x;
         }
         if (newPointerPosition.y > squareBase.y) {
+            this.y = squareBase.y;
             this.height = newPointerPosition.y - squareBase.y;
         }
         if (newPointerPosition.x < squareBase.x) {
